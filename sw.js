c2e47cc5504a5fb83411cb7b7236747f26253910
mek2424 <tom.horacek20@gmail.com>
2026-09-06 02:27:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bombito-v44';
+const CACHE = 'bombito-v46';
 const ASSETS = ['./', './index.html', './manifest.json', './chart.umd.min.js',
   './icons/icon-192.png', './icons/icon-512.png'];
 
